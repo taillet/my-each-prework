c49631db032cc7def431b953da4c3b337c79b79f
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:28:42 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  if array.size == 0
+    return nil
+  else
+    while i < array.size
+      yield(array[i])
+      i += 1
+    end
+  end
+  array
 end
