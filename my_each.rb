ec80e821ae2a39c6591980cb123911df671ec472
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:27:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,12 @@
 def my_each(array) # put argument(s) here
   # code here
-
+  i = 0
+if array.size == 0
+  return nil
+else  
   while i < array.size
     yield(array[i])
     i += 1
   end
-
   array
 end
